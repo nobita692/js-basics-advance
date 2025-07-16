07dbfe0a8121be05fe0d21025ee44d45db39b89b
ToufikRaja <119686867+nobita692@users.noreply.github.com>
2025-07-16 08:18:09 +0000

    variables

---
+++ basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 112321
+let accountEmial = "patanahi@gmail.com"
+var accountPass = "12322"
+accountCity = "Alwar"
+console.log(accountEmial);
+
+console.table([accountId,accountEmial,accountPass]);
+
+
