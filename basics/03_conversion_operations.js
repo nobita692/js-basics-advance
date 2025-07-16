d938df95312d06ac9fad580b860fd6949a75c9fd
ToufikRaja <119686867+nobita692@users.noreply.github.com>
2025-07-16 10:47:32 +0000

    conversion of data Types

---
+++ basics/03_conversion_operations.js
@@ -0,0 +1,24 @@
+//let score = "33qaq"
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);//It gives NaN means its Not a Number
+
+//"33" => 33
+// "33qwa" => NaN
+// true => 1, false => 0
+// "" => false , "Raja" =>true
+//let isLoggedIn = 1
+let isLoggedIn = "Raja"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+let someNum = 33
+let checkNum = String(someNum)
+console.log(checkNum);
+console.log(typeof(checkNum));
+
+
